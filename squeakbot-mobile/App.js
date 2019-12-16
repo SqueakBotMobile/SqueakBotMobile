506477ffe1d04afac9d7c68c0ca321e980ad988d
David Zheng <zhengdav@msu.edu>
2019-12-16 11:08:45 -0800

    proof of life, login page showing

---
+++ squeakbot-mobile/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>SqueakBot Mobile Version!</Text>
+      <Button title="Signup" onPress={() => console.log('login')}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
