f29fc2d39a035c9af33fbc8a16b76b75d2e0e06b
Julie Erlemeier <jmerlemeier@gmail.com>
2019-12-18 10:38:38 -0800

    added timer.

--- squeakbot-mobile/components/timer.js
+++ squeakbot-mobile/components/timer.js
@@ -10,15 +10,16 @@ class StopWatch extends Component {
 
     state = {
         timer: null,
+        minutes: '00',
         counter: '00',
-        miliseconds: '00',
+        milliseconds: '00',
         startDisabled: true,
         stopDisabled: false
     }
 
-
     constructor( props ) {
         super( props );
+
         this.onButtonStart = this.onButtonStart.bind(this);
         this.onButtonStop = this.onButtonStop.bind(this);
         this.onButtonClear = this.onButtonClear.bind(this);
@@ -34,54 +35,72 @@ class StopWatch extends Component {
     }
 
     start() {
-        var self = this;
+        // let self = this;
         let timer = setInterval(() => {
-            var num = (Number(this.state.miliseconds) + 1).toString(),
-                count = this.state.counter;
+            let num = (Number(this.state.milliseconds) + 1).toString();
+            let count = this.state.counter;
+            let minutes = this.state.minutes;
 
-            if( Number(this.state.miliseconds) == 99 ) {
+            if( Number(this.state.milliseconds) == 99 ) {
                 count = (Number(this.state.counter) + 1).toString();
                 num = '00';
             }
+            if( Number (this.state.counter) == 60 ) {
+              minutes = (Number(this.state.minutes) + 1).toString();
+              count ='00';
+            }
 
-            self.setState({
-                counter: count.length == 1 ? '0'+count : count,
-                miliseconds: num.length == 1 ? '0'+num : num
+            this.setState({
+                minutes: minutes.length == 1 ? '0' + minutes : minutes,
+                counter: count.length == 1 ? '0' + count : count,
+                milliseconds: num.length == 1 ? '0' + num : num,
             });
         }, 0);
         this.setState({timer});
     }
 
+
     onButtonStart() {
-      this.start();
-      this.setState({startDisabled: true, stopDisabled: false});
+
+        this.start();
+        this.setState({startDisabled: true, stopDisabled: false});
     }
+
 
     onButtonStop() {
-      clearInterval(this.state.timer);
-      this.setState({startDisabled: false, stopDisabled: true});
+        clearInterval(this.state.timer);
+        this.setState({startDisabled: false, stopDisabled: true});
     }
+
 
     onButtonClear() {
-      this.setState({
-        timer: null,
-        counter: '00',
-        miliseconds: '00'
-      });
+        this.setState({
+            timer: null,
+            minutes: null,
+            counter: '00',
+            milliseconds: '00'
+        });
     }
 
+
+
+
     render() {
-      return(
-        <View>  
-          <Text>{this.state.counter}</Text>
-          <Text>{this.state.miliseconds}</Text>
-          <Button title="Start" disabled="disabled"></Button>
-          <Button title="Stop" disabled="disabled"></Button>
-          <Button title="Clear" disabled="disabled"></Button>       
-        </View>
-      );
+        return(
+            <View>
+              <Text>{this.state.minutes}</Text>
+               <Text>{this.state.counter}</Text>
+                <Text>{this.state.milliseconds}</Text>
+{/*                 
+                    <Button title="Start" disabled="disabled"></Button>
+                    <Button title="Stop" disabled="disabled"></Button>
+                    <Button title="Clear" disabled="disabled"></Button>    */}
+            </View>
+        );
     }
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
@@ -103,5 +122,6 @@ const styles = StyleSheet.create({
         right: -50
     }
 });
+
 
 module.exports = StopWatch;
