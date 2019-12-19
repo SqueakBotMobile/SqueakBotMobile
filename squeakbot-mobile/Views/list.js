206dab4c9c27c9cf5b8e1509f622eafa1a4f2245
Julie Erlemeier <jmerlemeier@gmail.com>
2019-12-18 16:30:29 -0800

    fetched data from swapi.

--- squeakbot-mobile/Views/list.js
+++ squeakbot-mobile/Views/list.js
@@ -1,38 +1,67 @@
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TextInput, View, Image, Button, FlatList } from 'react-native';
+
+const useFetch = (url, options) => {
+  const [response, setResponse] = useState(null)
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url, options)
+        const json = await res.json()
+        setResponse(json)
+      } catch (e) {
+        setError(e)
+      }
+    }
+    fetchData()
+  }, [])
+  return { response, error }
+}
+
 
 export default (props) => {
   const {navigate} = props.navigation;
+
+  const res = useFetch("https://squeakbot-mobile.herokuapp.com/questions/challenges")
+  if(!res.response) return <Text>loading...</Text>
+
   return (
   <View style={styles.container}>
-    <Text>Our Questions</Text>
+    <Text style={styles.title}>List of Questions</Text>
+
+    <FlatList 
+        keyExtractor={item => item.name}
+        data={res.response.results}
+        renderItem={({ item }) => {
+          return (
+            <Text style={styles.textStyle}>
+              {item.name}
+            </Text>
+          )
+        }}
+      />
+
     <Button title="View Question" onPress={() => navigate('Question')}/>
   </View>
   );
-
 }
  
-
- 
-
-
 const styles = StyleSheet.create({
-  container: {
+  list: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50
   },
   textStyle: {
-    fontSize: 30,
-   color: 'teal',
+    fontSize: 20,
+    color: 'teal',
+    marginVertical: 25
   },
-  image: {
-    width: 200,
-    height:200,
-    resizeMode: 'stretch'
-  },
+  title: {
+    fontSize: 50,
+    color: 'orange',
+  }
 });
-
-
-  
